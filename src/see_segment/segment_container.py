e55a48942551d38a57a0f7fa135e315a8e398d2c
Kai <kai.pinckard@gmail.com>
2020-07-01 20:54:45 -0700

    Add support for command line arguments

--- src/see_segment/segment_container.py
+++ src/see_segment/segment_container.py
@@ -1,56 +1,121 @@
 if __name__ == "__main__":
+    import sys, getopt
     import imageio
     import numpy as np
-    from see import GeneticSearch, Segmentors
     import os
     import json
     import requests
     import random
+    import time
     from datetime import datetime
+    from see import GeneticSearch, Segmentors
+    #=====================================================================
+    """
+    Note: these are all the default values unless they are changed by
+    a command line argument these values will hold.
+    """
 
-    # 
-    POP_SIZE = 10
-    NUM_GENERATIONS = 10
-    INPUT_IMAGE = "input.jpg"
-    LABEL_IMAGE = "label.png"
-    SERVER_URL = "http://serverservice/update"  
+    # These values control the genetic search
+    pop_size = 10
+    num_gen = 5
 
-    rgb_url = "https://github.com/colbrydi/see-segment/blob/master/Image_data/Examples/Chameleon.jpg"
-    label_url = "https://github.com/colbrydi/see-segment/blob/master/Image_data/Examples/Chameleon_GT.png"
+    # These values describe wait times before image download attempts
+    initial_delay = 5
+    download_delay = 2
 
-    def make_download_command(url, save_as):
-        """
-        Used to construct a curl command to download
-        the image at the specified url.
-        """
-        return "wget -O " + str(save_as) + " " + str(url) + "?raw=true"
+    # The url of the server to contact
+    server_url = "http://0.0.0.0:8080"  
+
+    # These are relative urls and must be combined with the server url
+    rgb_url = "static/rgb.jpg"
+    label_url = "static/label.jpg"
+
+    #=====================================================================
+    # Handle command line arguments
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "p:g:l:h")
+    except getopt.GetoptError:
+        print("For help run:")
+        print("python segment_container.py -h")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-h":
+            print("flags:")
+            print("l: Location")
+            print("g: number of Generations")
+            print("p: Population size")
+            print("To run in a kubernetes cluster run:")
+            print("python segment_container.py -l cluster")
+            print("To run locally run:")
+            print("python segment_container.py -l local")
+            print("To run with a population size of 6 and to simulate 7 generations run:")
+            print("python segment_container.py -p 6 -g 7")
+            print("The defaults:")
+            print("By default location=local population=10 generations=5")
+            sys.exit()
+        elif opt == "-l":
+            if opt == "cluster":
+                server_url = "http://serverservice"
+        elif opt == "-p":
+            pop_size = int(opt)
+        elif opt == "-g":
+            num_gen = int(opt)
+    print("Running with parameters:")
+    print("population size =", str(pop_size))
+    print("number of generations =", str(num_gen))
+    print("server url =", server_url)
+    print("")
+
+
+
+    print("Attempting to download images in", initial_delay, "second(s)")
+    time.sleep(initial_delay)
+
+    # Create image urls
+    rgb_url = server_url + "/" + rgb_url
+    label_url = server_url + "/" + label_url
+
+    # Temporary, needs to be replaced with a means of determining the filetype
+    rgb_filename = "rgb.jpg"
+    label_filename = "label.jpg"
+
+    # Create image download commands
+    get_rgb = "wget -O " + str(rgb_filename) + " " + str(rgb_url)
+    get_label = "wget -O " + str(label_filename) + " " + str(label_url)
+
 
     # Download the images
-    os.system(make_download_command(rgb_url, INPUT_IMAGE))
-    os.system(make_download_command(label_url, LABEL_IMAGE))
+    download_status = os.system(get_rgb)
+    download_status = download_status + os.system(get_label)
+
+    while download_status != 0:
+        print("Download failed retrying in", download_delay, "second(s)")
+        time.sleep(download_delay)
+        download_status = os.system(get_rgb)
+        download_status = download_status + os.system(get_label)
 
     # Load the images
-    img = imageio.imread(INPUT_IMAGE)
-    gmask = imageio.imread(LABEL_IMAGE)
+    img = imageio.imread(rgb_filename)
+    gmask = imageio.imread(label_filename)
 
-    #only need on some images
+    # Only needed on some images
     # Convert the RGB 3-channel image into a 1-channel image
-    #gmask = (np.sum(gmask, axis=2) > 0)
+    # gmask = (np.sum(gmask, axis=2) > 0)
 
     # Create an evolver
-    my_evolver = GeneticSearch.Evolver(img, gmask, pop_size=POP_SIZE)
+    my_evolver = GeneticSearch.Evolver(img, gmask, pop_size=pop_size)
     
     # Conduct the genetic search
     population = None
 
-    for i in range(POP_SIZE):
+    for i in range(num_gen):
 
         # if population is uninitialized
         if population is None:
             population = my_evolver.run(ngen=1)
-
-        # Simulate a generation and store population in population variable
-        population = my_evolver.run(ngen=1, population=population)
+        else:
+            # Simulate a generation and store population in population variable
+            population = my_evolver.run(ngen=1, population=population)
 
         # Take the best segmentor from the hof and use it to segment the rgb image
         seg = Segmentors.algoFromParams(my_evolver.hof[0])
@@ -69,5 +134,6 @@ if __name__ == "__main__":
         data = json.dumps(data)
 
         # Send data to web server
+        server_update_url = server_url + "/update"
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        r = requests.post(SERVER_URL, data=json.dumps(data), headers=headers) 
+        r = requests.post(server_update_url, data=json.dumps(data), headers=headers) 
